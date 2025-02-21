749991a44130ae7e27f18262dbec5a09a6800467
yebh17 <sunnyrebels0@gmail.com>
2025-02-21 11:31:27 +0100

    Add elastisearch and filebeat to collect logs from the system and export to elasticsearch

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@ if [ "$1" == "-D" ]; then
 elif [ "$1" == "-d" ]; then
   echo "Running in detached mode"
   docker-compose -f orchestration/docker-compose.yml up -d
+# Running file beat agent on any server
+elif [ "$1" == "-f" ]; then
+  docker run -d --name=filebeat   --restart unless-stopped --user=root  -v /var/lib/docker/containers:/var/lib/docker/containers:ro   -v $(pwd)/orchestration/filebeat.yml:/usr/share/filebeat/filebeat.yml   docker.elastic.co/beats/filebeat:8.6.2 filebeat -e
 else
   echo "Spinning up the containers..."
   docker-compose -f orchestration/docker-compose.yml up

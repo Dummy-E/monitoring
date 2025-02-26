db64242abcdd63655d9e79afcd729205c05ecc8c
yebh17 <sunnyrebels0@gmail.com>
2025-02-26 18:32:54 +0100

    Add docker socket volume mount from host to container for picking up logs

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ elif [ "$1" == "-d" ]; then
   docker-compose -f orchestration/docker-compose.yml up -d
 # Running file beat agent on any server
 elif [ "$1" == "-f" ]; then
-  docker run -d --name=filebeat   --restart unless-stopped --user=root  -v /var/lib/docker/containers:/var/lib/docker/containers:ro   -v $(pwd)/orchestration/filebeat.yml:/usr/share/filebeat/filebeat.yml   docker.elastic.co/beats/filebeat:8.6.2 filebeat -e
+  docker run -d --name=filebeat   --restart unless-stopped --user=root  -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/run/docker.sock:/var/run/docker.sock:ro  -v $(pwd)/orchestration/filebeat.yml:/usr/share/filebeat/filebeat.yml   docker.elastic.co/beats/filebeat:8.6.2 filebeat -e
 else
   echo "Spinning up the containers..."
   docker-compose -f orchestration/docker-compose.yml up
